e9261fe2a1c40a705f557d57c700a8935e5931be
23 <23@DESKTOP-RLLM9KP>
2022-04-06 13:34:50 +0500

    Добавьте файлы проекта.

---
+++ Part3/Entity.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part3
+{
+    class PersonComparer : IComparer<person>
+    {
+        public int Compare(person p1, person p2)
+        {
+            if (p1 is null || p2 is null)
+                throw new ArgumentException("Некорректное значение параметра");
+            return p1.Place.Length - p2.Place.Length;
+        }
+    }
+    internal class person: IInit, IComparable<person>
+    {
+        protected string status;     // студент/студент-заочник/школьник
+        protected string place;     //место учебы: Школа/вуз
+        protected string gender;   //гендер 
+
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+        public string Place
+        {
+            get { return place; }
+            set { place = value; }
+        }
+
+        public string Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+        public person() : this("status", "place", "gender") { } //конструктор без параметров
+
+        public person(string status, string place, string gender) //конструктор с параметрами 
+        {
+            Status = status;
+            Place = place;
+            Gender = gender;
+        }
+        public virtual void ShowVirt() //виртуальный 
+        {
+            Console.WriteLine($"(Вирутальный метод)\nСтатус: {status}.\nМесто обучения: {place}.");
+        }
+        public void Show() //не виртуальный 
+        {
+            Console.WriteLine($"(Не вирутальный метод)\nСтатус: {status}.\nМесто обучения: {place}.");
+        }
+
+        static Random random = new Random();
+        public virtual void Init()
+        {
+            this.Status = random.Next(0, 100).ToString();
+            this.Gender = random.Next(0, 100).ToString();
+            this.Place = random.Next(0, 10000).ToString();
+        }
+
+        public override string ToString()
+        {
+            ShowVirt();
+            return "";
+        }
+
+        public int CompareTo(person other)
+        {
+            if (other is null) throw new ArgumentException("Некорректное значение параметра");
+            return Place.CompareTo(other.Place);
+        }
+    }
+    internal class schoolboy : person
+    {
+        public override void Init()
+        {
+            base.Init();
+            this.Progress = new Random().Next(0, 100).ToString();
+        }
+        string progress;
+        public string Progress
+        {
+            get { return progress; }
+            set { progress = value; }
+        }
+        public schoolboy() : base() //конструтор по-умолчанию с вызовом конструктора базового класса
+        {
+            progress = "undefined";
+        }
+        public schoolboy(string progress, string status, string place, string gender) : base("Школьник", place, "Женский") //конструтор с вызовом конструктора базового класса
+        {
+            Progress = progress;
+        }
+        public override void ShowVirt() //виртуальный 
+        {
+            base.ShowVirt();
+            Console.WriteLine($"Прогресс: {progress}.");
+        }
+        public new void Show() //не виртуальный 
+        {
+            base.Show();
+            Console.WriteLine($"Прогресс: {progress}.");
+        }
+    }
+    internal class StDistant : person
+    {
+        string form;
+        public override void Init()
+        {
+            base.Init();
+            Form = new Random().Next(0, 100).ToString();
+        }
+        public string Form
+        {
+            get { return form; }
+            set { form = value; }
+        }
+        public StDistant() : base() //конструтор по-умолчанию с вызовом конструктора базового класса
+        {
+            form = "undefined";
+        }
+        public StDistant(string form, string status, string place, string gender) : base("Студент-заочник", place, "Мужской") //конструтор с вызовом конструктора базового класса
+        {
+            Form = form;
+        }
+        public override void ShowVirt() //виртуальный 
+        {
+            base.ShowVirt();
+            Console.WriteLine($"Форма обучения: {form}.");
+        }
+        public new void Show() //не виртуальный 
+        {
+            base.Show();
+            Console.WriteLine($"Форма обучения: {form}.");
+        }
+        public override string ToString()
+        {
+            ShowVirt();
+            return "";
+        }
+    }
+
+    internal class student : person
+    {
+        public override void Init()
+        {
+            base.Init();
+            course = new Random().Next(0, 1);
+        }
+        int course;
+        public int Course
+        {
+            get { return course; }
+            set
+            {
+                if (value <= 0)
+                {
+                    course = 0;
+                    Console.WriteLine("Не учится в ВУЗе");
+                }
+                else course = value;
+            }
+        }
+        public student() : base() //конструтор по-умолчанию с вызовом конструктора базового класса
+        {
+            Course = 0;
+        }
+        public student(int course, string status, string place, string gender) : base("Студент", place, "Мужской") //конструтор с вызовом конструктора базового класса
+        {
+            Course = course;
+        }
+        public override void ShowVirt() //виртуальный 
+        {
+            base.ShowVirt();
+            Console.WriteLine($"Курс: {course}.");
+        }
+        public new void Show() //не виртуальный 
+        {
+            base.Show();
+            Console.WriteLine($"Курс: {course}.");
+        }
+    }   
+}
+
---
+++ Part3/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part3
+{
+    interface IInit
+    {
+        /// <summary>
+        /// Формирует объект с помощью ДСЧ
+        /// </summary>
+        /// <returns>Объект типа Т с случайными значениями свойств</returns>
+        void Init();
+    }
+
+    class InitClass : IInit
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public void Init()
+        {
+            Random random = new Random();
+            num1 = random.Next(0, 100);
+            num2 = random.Next(0, 100);
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine($"InitClass {num1} {num2}");
+            return base.ToString();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<IInit> inits = new List<IInit>();
+            //inits.Add(new person());
+            //inits.Add(new person());
+            //inits.Add(new student());
+            //inits.Add(new student());
+            //inits.Add(new StDistant());
+            //inits.Add(new StDistant());
+            //inits.Add(new schoolboy());
+            //inits.Add(new schoolboy());
+            //inits.Add(new InitClass());
+            //inits.Add(new InitClass());
+
+            //Console.WriteLine("До вызова метода Init");
+            //inits.ForEach(p => p.ToString());
+
+            //inits.ForEach(p => p.Init());
+            //Console.WriteLine("После вызова метода Init");
+            //inits.ForEach(p => p.ToString());
+            SortByComparer();
+            Console.WriteLine("---------");
+            SortWOComparer();
+            Console.WriteLine("---------");
+            FindObject();
+            Console.WriteLine("---------");
+            Copy();
+
+            
+           
+            Console.Read();
+        }
+        static void FindObject()
+        {
+            Console.WriteLine("Поиск элемента");
+            person[] people = new person[3];
+            people[0] = new schoolboy() { Place="Школа № 100"};
+            people[1] = new student(){ Place="Высшее учебное заведение № 100"};
+            people[2] = new StDistant() { Place = "Лицей № 1000000" };
+            Array.Sort(people);
+            foreach (var item in people)
+            {
+                item.Show();
+            }
+            var findObject = new schoolboy() { Place="Школа № 100"};
+            Console.WriteLine("Индекс искомого элемента: "+Array.BinarySearch(people, findObject));
+
+        }
+        static void SortWOComparer()
+        {
+            person[] people = new person[3];
+            people[0] = new schoolboy() { Place="Школа № 100"};
+            people[1] = new student(){ Place="Высшее учебное заведение № 100"};
+            people[2] = new StDistant() { Place = "Лицей № 1000000" };
+            Console.WriteLine("Сортировка без Comparer", ConsoleColor.Blue);
+            Console.WriteLine("Элементы до сортировки:");
+            for (int i = 0; i < people.Length; i++)
+            {
+                people[i].Show();
+            }
+            Console.WriteLine("Элементы после сортировки:");
+
+            Array.Sort(people);
+            for (int i = 0; i < people.Length; i++)
+            {
+                people[i].Show();
+            }
+
+
+        }
+
+        static void SortByComparer()
+        {
+            person[] people = new person[3];
+            people[0] = new schoolboy() { Place="Школа № 100"};
+            people[1] = new student(){ Place="Высшее учебное заведение № 100"};
+            people[2] = new StDistant() { Place = "Лицей № 1000000" };
+            Console.WriteLine("Сортировка c Comparer", ConsoleColor.Blue);
+            Console.WriteLine("Элементы до сортировки:");
+            for (int i = 0; i < people.Length; i++)
+            {
+                people[i].Show();
+            }
+            Console.WriteLine("Элементы после сортировки:");
+
+            Array.Sort(people, new PersonComparer());
+            for (int i = 0; i < people.Length; i++)
+            {
+                people[i].Show();
+            }
+
+        }
+        class InnerItem
+        {
+            public string name { get; set; }
+            public InnerItem(string name)
+            {
+                this.name = name;
+            }
+            public override string ToString()
+            {
+                return name;
+            }
+        }
+        class Item : ICloneable
+        {
+            string name { get; set; }
+            public InnerItem InnerItem { get; set; }
+            public Item(string name, InnerItem innerItem)
+            {
+                this.name = name;
+                this.InnerItem = innerItem;
+            }
+            public Item ShallowCopy()
+            {
+                return (Item)this.MemberwiseClone();
+            }
+            public object Clone()
+            {
+                return new Item("Clone "+this.name, new InnerItem(InnerItem.name));
+            }
+
+            public override string ToString()
+            {
+                return $"Name={name}, InnerItem name={this.InnerItem.ToString()}";
+            }
+        }
+        static void Copy()
+        {
+            var original = new Item("original", new InnerItem("inner item"));
+            Console.WriteLine("Cоздан объект: "+ original.ToString());
+            var ShallowClone = original.ShallowCopy();
+            Console.WriteLine("Поверхностно скопирован объект: "+ ShallowClone.ToString());
+            var DeepClone =(Item) original.Clone();
+            Console.WriteLine("Скопирован объект: "+ DeepClone.ToString());
+
+            // Меняем внутренний объект InnerItem
+            DeepClone.InnerItem.name = "123";
+            Console.WriteLine("Оригинал после изменения внутреннего объекта полного клона: "+ original.ToString());
+            ShallowClone.InnerItem.name = "123";
+            Console.WriteLine("Оригинал после изменения внутреннего объекта поверхностного клона: "+ original.ToString());
+
+
+        }
+    }
+}
